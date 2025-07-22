fcb6127723ec7bbf2cf8c7d5af92efc53d78c496
SamPaul Isaac <65417145+SamPaulIsaac@users.noreply.github.com>
2025-07-22 15:13:16 +0530

    Delete spring-boot-learning-hub/log-generation directory

--- spring-boot-learning-hub/log-generation/src/main/java/com/sam/logGeneration/repository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sam.logGeneration.repository;
-
-import com.sam.logGeneration.entity.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CustomerRepository extends JpaRepository<Customer, Integer> {
-}
