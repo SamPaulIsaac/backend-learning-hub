bdf0d4251906206e8ec4b15dd570e1e5673db759
SamPaul Isaac <65417145+SamPaulIsaac@users.noreply.github.com>
2025-07-22 20:22:21 +0530

    Delete spring-boot-learning-hub/student-registration-app directory

--- spring-boot-learning-hub/student-registration-app/studentRegistration/src/main/java/com/sam/studentRegistration/exception/EntityNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.sam.studentRegistration.exception;
-
-public class EntityNotFoundException extends RuntimeException{
-
-    public EntityNotFoundException(String message) {
-        super(message);
-    }
-}
