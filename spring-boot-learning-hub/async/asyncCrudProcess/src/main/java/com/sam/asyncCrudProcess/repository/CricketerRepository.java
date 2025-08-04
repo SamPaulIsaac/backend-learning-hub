eb9710cc84372fcec90d7f1407968dd2ed7676e7
SamPaul Isaac <65417145+SamPaulIsaac@users.noreply.github.com>
2025-08-04 22:33:48 +0530

    Delete spring-boot-learning-hub/async directory

--- spring-boot-learning-hub/async/asyncCrudProcess/src/main/java/com/sam/asyncCrudProcess/repository/CricketerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sam.asyncCrudProcess.repository;
-
-import com.sam.asyncCrudProcess.entity.Cricketer;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CricketerRepository extends JpaRepository<Cricketer, Long> {
-}
