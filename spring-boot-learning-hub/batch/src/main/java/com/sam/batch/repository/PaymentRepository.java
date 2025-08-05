92494260b37795e0f278355b6bea166b375eaf4b
SamPaul Isaac <65417145+SamPaulIsaac@users.noreply.github.com>
2025-08-05 18:49:41 +0530

    Delete spring-boot-learning-hub/batch directory

--- spring-boot-learning-hub/batch/src/main/java/com/sam/batch/repository/PaymentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sam.batch.repository;
-
-import com.sam.batch.entity.Payment;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PaymentRepository extends JpaRepository<Payment, Long> {
-}
